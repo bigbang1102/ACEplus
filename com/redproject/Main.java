ffb4b5c61a6d1014cb4d9280ea934e12dba2958f
TeerapongSJD <Saenjundee.2545@gmail.com>
2026-01-14 17:42:02 +0700

    Update Main.java

--- com/redproject/Main.java
+++ com/redproject/Main.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+//สวัสดี
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
